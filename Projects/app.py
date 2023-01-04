e8abc984993e1a8a249f32b244dbeb142ad95231
kmenon95 <79400523+kmenon95@users.noreply.github.com>
2023-01-04 09:48:31 +0530

    Create app.py

---
+++ Projects/app.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__, template_folder='C:/Users/kirti.menon.c/OneDrive - Accenture/Desktop/study_materials/Projects/templates')
+
+@app.route('/')
+def home():
+    return render_template('template.html')
+
+if __name__ == '__main__':
+    app.run(port=8080)
+%env FLASK_DEBUG=1
+%run app.py
